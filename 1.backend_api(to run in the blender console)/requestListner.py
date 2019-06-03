ed7856f7a30786962d943b79b1e95e5fcdcac698
Hizkias Abraham <ahizkias@gmail.com>
2019-06-03 11:34:45 +0300

    finalied

--- 1.backend_api(to run in the blender console)/requestListner.py
+++ 1.backend_api(to run in the blender console)/requestListner.py
@@ -1,3 +1,9 @@
+'''
+  There should be an http request listner that accepts requests from clients and 
+  responds with the location of the object in blender.
+  the current location of the object is held by variable  'currentLocation' which 
+  gets the location vector from 'bpy' object
+'''
 from http.server import BaseHTTPRequestHandler, HTTPServer
  
 # HTTPRequestHandler class
@@ -16,7 +22,7 @@ class BlenderLocationRequestListner(BaseHTTPRequestHandler):
         self.end_headers()
  
        # Current location of the cube from bpy 
-        currentLocation = currentLocation = bpy.data.scenes['Scene'].objects['Cube'].location
+        currentLocation = bpy.data.scenes['Scene'].objects['Cube'].location
         #  foramated location in to json
         formatedLocation = '{"x" : '+str(currentLocation[0])+', "y" : '+str(currentLocation[1])+', "z" : '+str(currentLocation[2])+'} '  
        
@@ -27,7 +33,6 @@ def run():
   print('starting server...')
  
   # Server settings
-  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
   server_address = ('localhost', 12000)
   httpd = HTTPServer(server_address, BlenderLocationRequestListner)
   print('running server...')
